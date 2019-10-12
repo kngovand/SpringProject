17e00ebc82a3f28f7b04c1500e4e017c5cda63d9
K Duc <kngovand@gmail.com>
2019-10-12 11:40:02 -0600

    oops - pushed before maven configuration

--- discovery-server/src/main/java/com/kngovand/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/kngovand/discoveryserver/DiscoveryServerApplication.java
@@ -2,8 +2,11 @@ package com.kngovand.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
